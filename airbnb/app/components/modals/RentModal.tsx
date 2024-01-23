f1c293e0e09c830370c2c340645fbb7c39cb7ab7
Lithara Perera <litharaperera2002@gmail.com>
2024-01-23 14:42:24 +0530

    modified

--- airbnb/app/components/modals/RentModal.tsx
+++ airbnb/app/components/modals/RentModal.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import useRentModal from "@/app/hooks/useRentModal";
 
@@ -7,77 +7,109 @@ import { useMemo, useState } from "react";
 import Heading from "../Heading";
 import { categories } from "../navbar/Categories";
 import CategorytInput from "../inputs/CategoryInput";
+import { FieldValues, useForm } from "react-hook-form";
 
 enum STEPS {
-    CATEGORY = 0,
-    LOCATION = 1,
-    INFO = 2,
-    IMAGES = 3,
-    DESCRIPTION = 4,
-    PRICE = 5,
+  CATEGORY = 0,
+  LOCATION = 1,
+  INFO = 2,
+  IMAGES = 3,
+  DESCRIPTION = 4,
+  PRICE = 5,
 }
 
 const RentModal = () => {
-    const rentModal = useRentModal();
+  const rentModal = useRentModal();
 
-    const [step, setStep] = useState(STEPS.CATEGORY);
+  const [step, setStep] = useState(STEPS.CATEGORY);
 
-    const onBack= () => {
-        setStep((value) => value - 1);
-    };
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    watch,
+    formState: { errors },
+    reset,
+  } = useForm<FieldValues>({
+    defaultValues: {
+      category: "",
+      location: null,
+      guestCount: 1,
+      roomCount: 1,
+      bathroomCount: 1,
+      imageSrc: "",
+      price: 1,
+      title: "",
+      description: "",
+    },
+  });
 
-    const onNext = () => {
-        setStep((value) => value + 1);
-    };
+  const category = watch("category");
 
-    const actionLabel = useMemo(() => {
-        if (step === STEPS.PRICE) {
-            return "Create";
-        }
+  const setCustomValue = (id: string, value: any) => {
+    setValue(id, value, {
+      shouldValidate: true,
+      shouldDirty: true,
+      shouldTouch: true,
+    });
+  };
 
-        return "Next";
-    }, [step]);
+  const onBack = () => {
+    setStep((value) => value - 1);
+  };
 
-    const secondaryActionLabel = useMemo(() => {
-        if (step === STEPS.CATEGORY) {
-            return undefined;
-        }
+  const onNext = () => {
+    setStep((value) => value + 1);
+  };
 
-        return "Back";
-    }, [step]);
+  const actionLabel = useMemo(() => {
+    if (step === STEPS.PRICE) {
+      return "Create";
+    }
 
-    let bodyContent = (
-        <div className="flex flex-col gap-8">
-            <Heading
-                title="Which of these best describes your place?"
-                subtitle="Pick a category"
+    return "Next";
+  }, [step]);
+
+  const secondaryActionLabel = useMemo(() => {
+    if (step === STEPS.CATEGORY) {
+      return undefined;
+    }
+
+    return "Back";
+  }, [step]);
+
+  let bodyContent = (
+    <div className="flex flex-col gap-8">
+      <Heading
+        title="Which of these best describes your place?"
+        subtitle="Pick a category"
+      />
+      <div className="grid grid-cols-1 md:grid-cols-2 max-h-[50vh] overflow-y-auto">
+        {categories.map((item) => (
+          <div key={item.label} className="col-span-1">
+            <CategorytInput
+              onClick={(category) => setCustomValue("category", category)}
+              selected={category === item.label}
+              label={item.label}
+              icon={item.icon}
             />
-            <div className="grid grid-cols-1 md:grid-cols-2 max-h-[50vh] overflow-y-auto">
-                {categories.map((item) => (
-                    <div key={item.label} className="col-span-1">
-                        <CategorytInput
-                            onClick={() => {}}
-                            selected={false}
-                            label={item.label}
-                            icon={item.icon}
-                        />
-                    </div>
-                ))}
-            </div>
-        </div>
-    )
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 
-    return (
-        <Modal
-            isOpen={rentModal.isOpen}
-            onClose={rentModal.onClose}
-            onSubmit={rentModal.onClose}
-            actionLabel="Submit"
-            secondaryActionLabel={secondaryActionLabel}
-            secondaryAction={step === STEPS.CATEGORY ? undefined : onBack}
-            title="Airbnb your home!"
-        />
-    );
-}
- 
+  return (
+    <Modal
+      isOpen={rentModal.isOpen}
+      onClose={rentModal.onClose}
+      onSubmit={rentModal.onClose}
+      actionLabel="Submit"
+      secondaryActionLabel={secondaryActionLabel}
+      secondaryAction={step === STEPS.CATEGORY ? undefined : onBack}
+      title="Airbnb your home!"
+    />
+  );
+};
+
 export default RentModal;
