ee62bf0828410986bfb66a1777dfb89573137093
Lithara Perera <litharaperera2002@gmail.com>
2024-01-23 12:52:43 +0530

    modidifed

--- airbnb/app/components/navbar/UserMenu.tsx
+++ airbnb/app/components/navbar/UserMenu.tsx
@@ -1,23 +1,29 @@
 "use client";
 
-import { AiOutlineMenu } from "react-icons/ai";
-import Avatar from "../Avatar";
 import { useCallback, useState } from "react";
-import MenuItem from "./MenuItem";
-import useRegisterModal from "@/app/hooks/useRegisterModal";
-import useLoginModal from "@/app/hooks/useLoginModal";
+import { AiOutlineMenu } from "react-icons/ai";
 import { signOut } from "next-auth/react";
-import { SafeUser } from "@/app/types";
+import { useRouter } from "next/navigation";
+
+import useLoginModal from "@/app/hooks/useLoginModal";
+import useRegisterModal from "@/app/hooks/useRegisterModal";
 import useRentModal from "@/app/hooks/useRentModal";
+import { SafeUser } from "@/app/types";
+
+import MenuItem from "./MenuItem";
+import Avatar from "../Avatar";
 
 interface UserMenuProps {
   currentUser?: SafeUser | null;
 }
 
 const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
+  const router = useRouter();
+
   const registerModal = useRegisterModal();
   const loginModal = useLoginModal();
   const rentModal = useRentModal();
+
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleOpen = useCallback(() => {
@@ -30,7 +36,7 @@ const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
     }
 
     rentModal.onOpen();
-  }, [currentUser, loginModal]);
+  }, [currentUser, loginModal, rentModal]);
 
   return (
     <div className="relative">
@@ -56,10 +62,10 @@ const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
           <div className="flex flex-col cursor-pointer">
             {currentUser ? (
               <>
-                <MenuItem onClick={() => {}} label="My trips" />
-                <MenuItem onClick={() => {}} label="My favorites" />
-                <MenuItem onClick={() => {}} label="My reservations" />
-                <MenuItem onClick={() => {}} label="Airbnb my home" />
+                <MenuItem onClick={() => router.push('/trips')} label="My trips" />
+                <MenuItem onClick={() => router.push('/favorites')} label="My favorites" />
+                <MenuItem onClick={() => router.push('/reservations')} label="My reservations" />
+                <MenuItem onClick={() => router.push('/properties')} label="Airbnb my home" />
                 <hr />
                 <MenuItem onClick={() => signOut()} label="Logout" />
               </>
@@ -77,5 +83,3 @@ const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
 };
 
 export default UserMenu;
-
-//3:20:06
